f48fedc98181da98f252288a27de8d203130290f
shalvah <diakon.ng@gmail.com>
2018-03-17 18:08:46 +0100

    Create user on Chatkit after user sign up

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\User;
+use Chatkit\Laravel\Facades\Chatkit;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -27,6 +30,10 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        //
+        User::created(function (User $user) {
+            // create Chatkit user
+            $userId = $user->email;
+            Chatkit::createUser($userId, $user->name);
+        });
     }
 }
